68a25ad8ef718eceec202d0b35f789341b140940
Dan Seiders <dseiders333@gmail.com>
2020-10-09 13:06:20 -0400

    basic html layout started

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-console.log($)
-console.log('hello b')
+
+ const buildBoards = () =>{
+   for(let i = 1; i <= 100; i++){
+    const $boardSquares = $('<div class="squares">')
+    const $board = $('.board')
+  $board.append($boardSquares)
+  }
+}
+
 
 $(()=> {
+  buildBoards()
     
 })
