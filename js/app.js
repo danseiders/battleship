a0777ca472e3a7278f59a20c911a0a7d0fc8b363
Dan Seiders <dseiders333@gmail.com>
2020-10-12 22:12:36 -0400

    start game button completed. computer ships randomly place and disappear off list.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-
-   
-   //this builds the two game boards, assigin a letter and number to the class for each square on the board. Letters are rows, numbers are columns. 
+//this builds the two game boards, assigin a letter and number to the class for each square on the board. Letters are rows, numbers are columns. 
 const rowLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
    
 const buildBoard = (user) =>{
@@ -8,7 +6,8 @@ const buildBoard = (user) =>{
     for(let j = 0; j < rowLetters.length; j++){
     const letter = rowLetters[j];
     const $boardSquares = $(`<div class="${user}_board_squares" column="${letter}" row="${i}" id="${letter} ${i}">`).on('click', (e) => {
-    $(e.target).css('background-color', 'red') //for now this changes the square to the color red, although I need to add a function that will check the contents of the box for a ship, then change the color to white if miss, red if hit. 
+    $(e.target).css('background-color', 'white')
+    .css('border-radius', '10px') 
     })
     const $board = $(`#${user}_board`)
     $board.append($boardSquares)
@@ -16,14 +15,33 @@ const buildBoard = (user) =>{
   }
 }
 
+//this section randomly places the computers ships and makes them disappear with opacity. the arrays below are pixel values
+const carrierTop = [0, 17, 34, 51, 68, 85, 102, 119, 136, 153];
+const battleshipTop = [-17, 0, 17, 34, 51, 68, 85, 102, 119, 136];
+const cruiserTop = [-34, -17, 0, 17, 34, 51, 68, 85, 102, 119];
+const submarineTop = [-51, -34, -17, 0, 17, 34, 51, 68, 85, 102];
+const destroyerTop = [-68, -51, -34, -17, 0, 17, 34, 51, 68, 85];
+
 const placePlayer1Ships = () => {
-  $('#P1carrier').css('left', (`${((Math.floor(Math.random() * 6) +7) * 16)}px`))
-  $('#P1carrier').css('top', (`${(Math.floor(Math.random() * 10) * 17)}px`))
+  $('#P1Carrier').css('top', (`${carrierTop[Math.floor(Math.random() * 10)]}px`))
+    .css('left', (`${((Math.floor(Math.random() * 6) +7) * 16)}px`))
+  $('#P1Battleship').css('top', (`${battleshipTop[Math.floor(Math.random() * 10)]}px`))
+    .css('left', (`${((Math.floor(Math.random() * 6) +8) * 16)}px`))
+  $('#P1Cruiser').css('top', (`${cruiserTop[Math.floor(Math.random() * 10)]}px`))
+    .css('left', (`${((Math.floor(Math.random() * 6) +9) * 16)}px`))
+  $('#P1Submarine').css('top', (`${submarineTop[Math.floor(Math.random() * 10)]}px`))
+    .css('left', (`${((Math.floor(Math.random() * 6) +9) * 16)}px`))
+  $('#P1Destroyer').css('top', (`${destroyerTop[Math.floor(Math.random() * 10)]}px`))
+    .css('left', (`${((Math.floor(Math.random() * 6) +10) * 16)}px`))
+  $('.player1ShipContents').css('opacity',  '0.01')
 }
 
 //this starts the game after user clicks start button. 
 const startGame = () => {
-  console.log('clicked!')
+  
+  $('.start_button').remove()
+  $('.player1_board_squares').css('background-color', 'cadetblue').css('border-radius', '0')
+  $('.player2_board_squares').css('background-color', 'cadetblue')
   $('.P1ship').draggable('disable')
   $('.P2ship').draggable('disable')
   placePlayer1Ships();  
@@ -35,10 +53,9 @@ const startGame = () => {
 
 $(()=> {
   const $startButton = $('.start_button')
-  
   buildBoard('player1') //builds player1 board
   buildBoard('player2') //builds player 2 board
-  $($startButton).on('click', startGame) //when start button clicked, ships cant be moved
+  $($startButton).on('click', startGame)//when start button clicked, ships cant be moved
 
   
   
@@ -100,10 +117,7 @@ $(()=> {
 
 //LOGIC
 //make modal with directions
-// when user hits "start game" player 1 ships get randomly placed
-// player 1 ships disappear from "player 1 ships"
-//user fires shots by clicking on player 1 board.
-// missed shots are changed to white circle inside of square
+
 // hits are changed to red circle inside of square
 // player 1 automatically fires after user fires
 // player 1 shots are random on board. (can try to make this a little smarter if I have time)
