5ff0f453289ccdccdde2d49cfda664b4e69c8a96
Dan Seiders <dseiders333@gmail.com>
2020-10-12 22:57:40 -0400

    computer ship divs turn red when clicked

--- js/app.js
+++ js/app.js
@@ -15,13 +15,14 @@ const buildBoard = (user) =>{
   }
 }
 
-//this section randomly places the computers ships and makes them disappear with opacity. the arrays below are pixel values
+
+
+//this section randomly places the computers ships and makes them disappear with opacity. the arrays below are pixel values for height. 
 const carrierTop = [0, 17, 34, 51, 68, 85, 102, 119, 136, 153];
 const battleshipTop = [-17, 0, 17, 34, 51, 68, 85, 102, 119, 136];
 const cruiserTop = [-34, -17, 0, 17, 34, 51, 68, 85, 102, 119];
 const submarineTop = [-51, -34, -17, 0, 17, 34, 51, 68, 85, 102];
 const destroyerTop = [-68, -51, -34, -17, 0, 17, 34, 51, 68, 85];
-
 const placePlayer1Ships = () => {
   $('#P1Carrier').css('top', (`${carrierTop[Math.floor(Math.random() * 10)]}px`))
     .css('left', (`${((Math.floor(Math.random() * 6) +7) * 16)}px`))
@@ -38,37 +39,55 @@ const placePlayer1Ships = () => {
 
 //this starts the game after user clicks start button. 
 const startGame = () => {
-  
-  $('.start_button').remove()
-  $('.player1_board_squares').css('background-color', 'cadetblue').css('border-radius', '0')
+  const $shipContents = $('.player1ShipContents')
+  $('.start_button').remove() //removes start button
+  $('.player1_board_squares').css('background-color', 'cadetblue')
+    .css('border-radius', '0') //resets player1 board
   $('.player2_board_squares').css('background-color', 'cadetblue')
-  $('.P1ship').draggable('disable')
-  $('.P2ship').draggable('disable')
-  placePlayer1Ships();  
+    .css('border-radius', '0') //resets player2 board
+  $('.P1ship').draggable('disable') //disables drag functionality on player 1
+  $('.P2ship').draggable('disable') //disables drag functionality on player 2
+  placePlayer1Ships();  //runs start sequence
+  $shipContents.on('click', (e) => {
+    $(e.target).css('background-color', 'red')
+    .css('border-radius', '10px') 
+    .css('opacity', '1')
+})
+  
 }
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////
 ///JQUERY ON LOAD///
 ////////////////////
 
+
 $(()=> {
   const $startButton = $('.start_button')
+  
   buildBoard('player1') //builds player1 board
   buildBoard('player2') //builds player 2 board
   $($startButton).on('click', startGame)//when start button clicked, ships cant be moved
+  
+//   const $shipContents = $('.player1ShipContents')
+//   $shipContents.on('click', (e) => {
+//     $(e.target).css('background-color', 'red')
+//     .css('border-radius', '10px') 
+// })
 
   
   
 
-  
-  
-
-//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////
   /////////////////////////////
   ///JQuery UI Drag and Drop///
   /////////////////////////////
   $(function() {  
     const $carrier = $(".carrier")
+    const $column = document.getElementById('column')
+    const $row = document.getElementById('row')
     
     //this makes each ship draggable to specific pixel grid and changes cursor to crosshairs. 
     $( ".P1ship").draggable({ 
@@ -84,22 +103,17 @@ $(()=> {
      });
   
     //this only lets the user drop their ships on only their board, while adding the class of where its dropped.
-
-    const $column = document.getElementById('column')
-    const $row = document.getElementById('row')
-
     $( ".player1_board_squares" ).droppable({
       accept: ".P1ship",
       drop: function( e, ui ) {
         $(this).addClass( "P1_placed_ship" ).find(".player1_board_squares")
-     
       }
     });
+
     $( ".player2_board_squares" ).droppable({
       accept: ".P2ship",
       drop: function( e, ui ) {
-        $( this ).addClass( "P2_placed_ship" ).find( ".player2_board_squares" )
-            
+        $( this ).addClass( "P2_placed_ship" ).find( ".player2_board_squares" )        
       }
     });
   });   
