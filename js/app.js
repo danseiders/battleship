af3bbc64f04ac8fe720af966f20f6dc201c0ed21
Dan Seiders <dseiders333@gmail.com>
2020-10-13 23:25:03 -0400

    found a way to label where the user ships are placed

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ let player1Data = {
     p1Cruiser: 3,
     p1Submarine: 3,
     p1Destroyer: 2,
-    
     destroyedShips: [],
     shotsFired: 0,
     shotsMissed: 0,
@@ -21,6 +20,14 @@ let player2Data = {
     p2Cruiser: 3,
     p2Submarine: 3,
     p2Destroyer: 2,
+    shipLocation: {
+      p1Carrier: [],
+      p1Battleship: [],
+      p1Cruiser: [],
+      p1Submarine: [],
+      p1Destroyer: [],
+
+    },
     destroyedShips: [],
     shotsFired: 0,
     shotsMissed: 0,
@@ -109,8 +116,6 @@ const userFires = (e) => {
     .css('border-radius', '10px') 
     setTimeout(computerFiresBack, 500) 
   } else if(hit === true) {
-   
-    // console.log((player2Data.battleships).location)
     console.log(`HIT!`)
     $(e.target).css('background-color', 'red')
     .css('border-radius', '10px')
@@ -118,14 +123,65 @@ const userFires = (e) => {
   }
 }
 
+//fires back after user shoots. a random div is picked by column(letter) vs row(number). The div shot at changes colors to white.
 const computerFiresBack = () => {
   console.log('COMPUTER FIRES BACK!')
- const column = columnLetters[Math.floor(Math.random() * 10)]
- const row = Math.floor(Math.random() * 10)
- const event = document.getElementById(`${column}${row}`)
-
- console.log(event)
+  const column = columnLetters[Math.floor(Math.random() * 10)]
+  const row = Math.floor(Math.random() * 10)
+  const $square = $(document.body.children[0].children[2].children[3].children[`${column}${row}`])
+  $square.css('background-color', 'white')
+  $square.css('border-radius', '10px')
 }
+
+
+
+// const p1AddShipLocation = (ship, location)=>{
+//   if(ship === "p1Carrier") {
+//     player1ShipLocation.p1Carrier = location
+//     console.log(userShipLocation)
+//   } else if (ship === "p1Battleship"){
+//     player1ShipLocation.p1Battleship = location   
+//     console.log(userShipLocation)
+//   } else if (ship === 'p1Cruiser'){
+//     player1ShipLocation.p1Cruiser = location   
+//     console.log(userShipLocation)
+//   } else if (ship === 'p1Submarine'){
+//     player1ShipLocation.p1Submarine = location   
+//     console.log(userShipLocation)
+//   } else if (ship === 'p1Destroyer'){
+//     player1ShipLocation.p1Destroyer = location   
+//     console.log(userShipLocation)
+//   }
+// }
+const p2AddShipLocation = (ship, location)=>{
+  const locationLetter = location[0]
+  const locationNumber = Number(location[1])
+  
+  if(ship === "p2Carrier") {
+    player2Data.shipLocation.p2Carrier = [
+      locationLetter + (locationNumber - 2), 
+      locationLetter + (locationNumber - 1), 
+      location, 
+      locationLetter + (locationNumber + 1), 
+      locationLetter + (locationNumber + 2)]
+    console.log(player2Data.shipLocation.p2Carrier)
+    console.log(locationLetter)
+    console.log(locationNumber + 100)
+  } else if (ship === "p2Battleship"){
+    player2ShipLocation.p2Battleship = location   
+    console.log(player2ShipLocation)
+  } else if (ship === 'p2Cruiser'){
+    player2ShipLocation.p2Cruiser = location   
+    console.log(player2ShipLocation)
+  } else if (ship === 'p2Submarine'){
+    player2ShipLocation.p2Submarine = location   
+    console.log(player2ShipLocation)
+  } else if (ship === 'p2Destroyer'){
+    player2ShipLocation.p2Destroyer = location   
+    console.log(player2ShipLocation)
+  }
+}
+
 
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -142,48 +198,57 @@ $(()=> {
   $($startButton).on('click', startGame)//when start button clicked, ships cant be moved
   
 
-  
-  
 
 //////////////////////////////////////////////////
   /////////////////////////////
   ///JQuery UI Drag and Drop///
   /////////////////////////////
   $(function() {  
-    const $Carrierrier = $(".Carrierrier")
-    const $column = document.getElementById('column')
-    const $row = document.getElementById('row')
-    
-    //this makes each ship draggable to specific pixel grid and changes cursor to crosshairs. 
+    //makes each ship draggable to each playerboard at correct pixel and changes cursor to crosshairs. 
     $( ".p1ship").draggable({ 
       cursor: "crosshair",
       revert: "invalid",
       grid: [ 16, 17 ],
      });
-    
-     $( ".p2ship" ).draggable({ 
+    $( ".p2ship").draggable({ 
       cursor: "crosshair",
       revert: "invalid",
       grid: [ 16, 17 ],
      });
+    
+   
   
-    //this only lets the user drop their ships on only their board, while adding the class of where its dropped.
-    $( ".player1_board_squares" ).droppable({
-      accept: ".p1ship",
-      drop: function( e, ui ) {
-        $(this).addClass( "p1_placed_ship" ).find(".player1_board_squares")
-      }
-    });
+    //lets the user drop their ships on only their board, while adding the class of where its dropped.
+    // $( ".player1_board_squares" ).droppable({
+    //   accept: ".p1ship",
+    //   drop: function( e, ui ) {
+    //     $(this).addClass( "p1_placed_ship" ).find(".player1_board_squares")
+    //   }
+    // });
+
+    // $( ".player1_board_squares" ).droppable({
+    //   accept: ".p1ship",
+    //   drop: function(e) {
+    //    const location = e.target.id //gets the div the ship is dropped on
+    //    const ship = document.getElementById(`${e.originalEvent.target.id}`).parentElement.id //finds the id of the ship dropped
+    //    p1AddShipLocation(ship, location)
+    //    $(this).addClass(`${e.originalEvent.target.id}`).find( ".player1_board_squares" )        
+    //   },
+    // });
 
     $( ".player2_board_squares" ).droppable({
       accept: ".p2ship",
-      drop: function( e, ui ) {
-        $( this ).addClass( "p2_placed_ship" ).find( ".player2_board_squares" )        
-      }
+      drop: function(e) {
+       const location = e.target.id //gets the div the ship is dropped on
+       const ship = document.getElementById(`${e.originalEvent.target.id}`).parentElement.id //finds the id of the ship dropped
+       p2AddShipLocation(ship, location)
+       $(this).addClass(`${e.originalEvent.target.id}`).find( ".player2_board_squares" )        
+      },
     });
   });   
 })
 //////////////////////////////////////////////////////////////////////////////////
+
 
 ////////////////
 ///to do list///
